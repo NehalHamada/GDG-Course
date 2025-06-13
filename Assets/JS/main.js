9ca4be426a54b65005db4a421a61a9089dcbbdf6
Nehal Hamada <116380963+NehalHamada@users.noreply.github.com>
2025-06-13 14:04:25 +0300

    push

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,8 +1,11 @@
-// Task 4 
+// Task 4
 // const button = document.querySelector(".non-active");
 // button.addEventListener("click", function () {
 //   document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
 // });
 
-
 // Task 5
+document.querySelector(".box-1").style.backgroundImage =
+  "url('/Assets/imgs/b17.jpg')";
+document.querySelector(".box-2").style.backgroundImage =
+  "url('/Assets/imgs/b10.jpg')";
