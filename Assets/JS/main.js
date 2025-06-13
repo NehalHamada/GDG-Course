a093802c564c85146fc40b99f3d812a1ff6be33c
Nehal Hamada <116380963+NehalHamada@users.noreply.github.com>
2025-06-13 12:47:27 +0300

    pUsH

---
+++ Assets/JS/main.js
@@ -0,0 +1,8 @@
+// Task 4 
+// const button = document.querySelector(".non-active");
+// button.addEventListener("click", function () {
+//   document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+// });
+
+
+// Task 5
